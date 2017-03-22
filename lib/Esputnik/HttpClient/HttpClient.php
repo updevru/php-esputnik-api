4bee2b33363dad9033f41673d257307520f0b4d6
Ladygin Sergey <webcoderu@gmail.com>
2017-03-22 22:52:37 +0300

    [+] Убираем принудительный debug

--- lib/Esputnik/HttpClient/HttpClient.php
+++ lib/Esputnik/HttpClient/HttpClient.php
@@ -34,7 +34,6 @@ class HttpClient
         $client = $client ?: new GuzzleClient([
             'base_uri' => sprintf('%s%s/', $this->options['base_url'], $this->options['api_version']),
             'auth' => [$this->options['login'], $this->options['password']],
-            'debug' => true,
             'headers' => $this->headers,
         ]);
 
